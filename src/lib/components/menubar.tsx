6da8f2ca27890866255ff1ae51c8a48e740eab00
Amit Dubey <iamsaagar@yahoo.com>
2024-04-10 13:21:54 +0100

    Updated cart icon

--- src/lib/components/menubar.tsx
+++ src/lib/components/menubar.tsx
@@ -39,6 +39,14 @@ const MenuBar: FC<MenuBarProps> = ({ menuItems, selectedMenuId, username }) => {
           />
         </Navbar.Brand>
         {isAdmin && <p className="text-light">&nbsp;Admin</p>}
+        <Nav.Link href={PageRoutes.Cart} className="d-xs-block d-sm-none">
+          <h5 className="position-relative">
+            <i className="icon-handbag"></i>
+            {products.length > 0 && (
+              <sub className="badge cart-sub">{products.length}</sub>
+            )}
+          </h5>
+        </Nav.Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav bar-opener">
           <i className="icon-menu"></i>
         </Navbar.Toggle>
@@ -51,10 +59,12 @@ const MenuBar: FC<MenuBarProps> = ({ menuItems, selectedMenuId, username }) => {
               {(menuItems ?? []).map((item) => (
                 <Nav.Link href={item.route}>{item.label}</Nav.Link>
               ))}
-              <Nav.Link href={PageRoutes.Cart}>
+              <Nav.Link href={PageRoutes.Cart} className="d-sm-block d-none">
                 <h5 className="position-relative">
                   <i className="icon-handbag"></i>
-                  <sub className="badge cart-sub">{products.length}</sub>
+                  {products.length > 0 && (
+                    <sub className="badge cart-sub">{products.length}</sub>
+                  )}
                 </h5>
               </Nav.Link>
               <Nav.Link
@@ -71,82 +81,6 @@ const MenuBar: FC<MenuBarProps> = ({ menuItems, selectedMenuId, username }) => {
         )}
       </Container>
     </Navbar>
-    // <header id="mt-header" className="style4 shadow fixed navbar-expand-sm">
-    //   <div className="mt-bottom-bar">
-    //     <div className="container-fluid">
-    //       <div className="row">
-    //         <div className="col-xs-12">
-    //           <div className="d-in-flex align-items-end w-200">
-    //             <Link to={PageRoutes.Home}>
-    //               <img
-    //                 className="mt-logo-wish"
-    //                 src="/assets/images/W4R_Website_Logo-04.jpg"
-    //                 alt=""
-    //               />
-    //             </Link>
-    //             {isAdmin && <p className="text-light">&nbsp;Admin</p>}
-    //           </div>
-
-    //           {menuItems.length > 0 && (
-    //             <ul className="mt-icon-list">
-    //               <li className="hidden-lg hidden-md">
-    //                 <Link
-    //                   to=""
-    //                   className="bar-opener navbar-toggler"
-    //                   data-bs-toggle="collapse"
-    //                   data-bs-target="#navbarSupportedContent"
-    //                   aria-controls="navbarSupportedContent"
-    //                   aria-expanded="false"
-    //                   aria-label="Toggle navigation"
-    //                   title="mobile menu"
-    //                 >
-    //                   <span className="bar"></span>
-    //                   <span className="bar small"></span>
-    //                   <span className="bar"></span>
-    //                 </Link>
-    //               </li>
-    //               <li className="drop">
-    //                 <Link
-    //                   to={PageRoutes.Cart}
-    //                   className="cart-opener"
-    //                   title="Cart"
-    //                 >
-    //                   <span className="icon-handbag"></span>
-    //                   <span className="num">{products.length}</span>
-    //                 </Link>
-    //               </li>
-    //               <li>
-    //                 <Link
-    //                   to={PageRoutes.Login}
-    //                   onClick={handleSignOutClick}
-    //                   className="cart-opener"
-    //                   title="Log out"
-    //                 >
-    //                   <span className="icon-power"></span>
-    //                 </Link>
-    //               </li>
-    //             </ul>
-    //           )}
-    //           <nav
-    //             id="navbarSupportedContent"
-    //             className="collapse navbar-collapse"
-    //           >
-    //             <ul className="navbar-nav">
-    //               {(menuItems ?? []).map((item) => (
-    //                 <li key={item.id} className="nav-item">
-    //                   <Link to={item.route} className="nav-link">
-    //                     {item.label}{" "}
-    //                   </Link>
-    //                 </li>
-    //               ))}
-    //             </ul>
-    //           </nav>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <span className="mt-side-over"></span>
-    // </header>
   );
 };
 
